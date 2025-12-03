1dbcd196d38168de8279a103fab719d16a1fdc9c
Lydia Wong <lydiawong@Lydias-MacBook-Air.local>
2025-12-03 12:07:00 +0800

    made changes to read_file

--- get_next_line.c
+++ get_next_line.c
@@ -4,32 +4,25 @@ char	*read_file(int fd, char *res)
 {
 	char	*buffer;
 	int		bytes_read;
-	char	*dest;
 
 	buffer = ft_calloc(buf_size + 1, sizeof(char));
-	dest = ft_calloc(1, sizeof(char));
-	if (!buffer || !dest)
+	if (!buffer)
 		return (NULL);
-	if (res)
-	{
-		dest = ft_strljoin(dest, res, ft_strlen(res));
-		res = NULL;
-	}
+	if (!res)
+		res = ft_calloc(1, sizeof(char));
 	while (1)
 	{
 		bytes_read = read(fd, buffer, buf_size);
 		if (bytes_read <= 0)
 			break ;
-        buffer[bytes_read] = '\0';
-		dest = ft_strljoin(dest, buffer, bytes_read);
+		buffer[bytes_read] = '\0';
+		res = ft_strljoin(res, buffer, bytes_read);
 		if (ft_strchr(buffer, '\n'))
 			break ;
 	}
 	free(buffer);
-	return (dest);
+	return (res);
 }
-//if res is empty, just initialize ta shi empty string
-//REUSE RES
 
 char	*get_line(char *buffer)
 {
@@ -40,9 +33,9 @@ char	*get_line(char *buffer)
 		return (NULL);
 	i = 0;
 	while (buffer[i] && buffer[i] != '\n')
-        i++;
-    if (buffer[i] == '\n')
-        i++; /* to include the newline */
+		i++;
+	if (buffer[i] == '\n')
+		i++; /* to include the newline */
 	dest = ft_substr(buffer, 0, i);
 	return (dest);
 }
@@ -54,12 +47,12 @@ char	*following_line(char *buffer)
 
 	if (!buffer)
 		return (NULL);
-    i = 0;
+	i = 0;
 	while (buffer[i] && buffer[i] != '\n')
 		i++;
-    i++; /* skip newline */
+	i++; /* skip newline */
 	res = ft_substr(buffer, i, ft_strlen(buffer) - i);
-    free (buffer);
+	free(buffer);
 	return (res);
 }
 
@@ -68,11 +61,11 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*res;
 
-    if (fd < 0)
-        return (NULL);
+	if (fd < 0)
+		return (NULL);
 	buffer = read_file(fd, buffer);
-    if (!buffer)
-        return (NULL);
+	if (!buffer)
+		return (NULL);
 	res = get_line(buffer);
 	buffer = following_line(buffer);
 	return (res);
@@ -80,12 +73,25 @@ char	*get_next_line(int fd)
 
 int	main(void)
 {
-	int	fd;
-	int	count;
-
-	fd = open("text.txt", O_RDONLY);
+	int fd = open("text.txt", O_RDONLY);
+	int fd1 = open("meow.txt", O_RDONLY);
+	int fd2 = open("woof.txt", O_RDONLY);
 	printf("1ST: %s", get_next_line(fd));
 	printf("2ND: %s", get_next_line(fd));
 	printf("3RD: %s", get_next_line(fd));
+	printf("4TH: %s", get_next_line(fd));
+	printf("5TH: %s", get_next_line(fd));
+	// printf("\n---------------\n");
+	// printf("1ST: %s", get_next_line(fd1));
+	// printf("2ND: %s", get_next_line(fd1));
+	// printf("3RD: %s", get_next_line(fd1));
+	// printf("4TH: %s", get_next_line(fd1));
+	// printf("5TH: %s", get_next_line(fd1));
+	// printf("\n---------------\n");
+	// printf("1ST: %s", get_next_line(fd2));
+	// printf("2ND: %s", get_next_line(fd2));
+	// printf("3RD: %s", get_next_line(fd2));
+	// printf("4TH: %s", get_next_line(fd2));
+	// printf("5TH: %s", get_next_line(fd2));
 	close(fd);
 }
